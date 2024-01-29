1b18b95349d08bb1a57f180ad70e34880c00588d
Mikhail Zaitsau <mikhailzaitsau.work@gmail.com>
2024-01-29 22:04:58 +0300

    Add gestures logic

--- lib/gesture.rb
+++ lib/gesture.rb
@@ -1,21 +1,22 @@
 module Gesture
-  def initialize(number)
+  def self.rock(gesture)
+    %w[scissors lizard].include?(gesture)
   end
 
-  private
-
-  def rock
+  def self.paper(gesture)
+    %w[rock spock].include?(gesture)
   end
 
-  def paper
+  def self.scissors(gesture)
+    %w[paper lizard].include?(gesture)
   end
 
-  def scissors
+  def self.spock(gesture)
+    %w[scissors rock].include?(gesture)
   end
 
-  def spock
-  end
-
-  def lizard
+  def self.lizard(gesture)
+    %w[paper spock].include?(gesture)
   end
 end
+
